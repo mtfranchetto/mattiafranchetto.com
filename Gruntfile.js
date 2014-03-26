1c7e4c6389cc6526fdc9a637a0a4d37eb278b7e9
Mattia Franchetto <mt.franchetto@gmail.com>
2014-03-27 00:38:08 +0100

    Edit build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,10 @@
 module.exports = function(grunt) {
 
     var BUILD_DIR = 'public/',
-        SOURCE_DIR = 'client/';
+        SOURCE_DIR = 'public/';
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        clean: [
-            BUILD_DIR + 'js',
-            BUILD_DIR + 'css',
-            BUILD_DIR + 'img',
-            BUILD_DIR + 'templates',
-            BUILD_DIR + '404.html',
-            BUILD_DIR + 'apple-touch-icon.png',
-            BUILD_DIR + 'favicon.ico',
-            BUILD_DIR + 'humans.txt',
-            BUILD_DIR + 'robots.txt',
-            BUILD_DIR + 'sitemap.xml'
-        ],
         uglify: {
           options: {
             banner: '/*! <%= pkg.name %> <%= pkg.version %>  / <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -40,36 +28,15 @@ module.exports = function(grunt) {
                     ]
                 }
             }
-        },
-        copy: {
-          main: {
-            files: [
-              { expand: true, cwd: SOURCE_DIR, src: ['templates/**'], dest: BUILD_DIR },
-              { expand: true, cwd: SOURCE_DIR, src: ['css/responsive.css'], dest: BUILD_DIR },
-              { expand: true, cwd: SOURCE_DIR, src: ['img/**'], dest: BUILD_DIR },
-              { expand: true, cwd: SOURCE_DIR, src: ['favicon.ico'], dest: BUILD_DIR},
-              { expand: true, cwd: SOURCE_DIR, src: ['404.html'], dest: BUILD_DIR},
-              { expand: true, cwd: SOURCE_DIR, src: ['apple-touch-icon.png'], dest: BUILD_DIR},
-              { expand: true, cwd: SOURCE_DIR, src: ['humans.txt'], dest: BUILD_DIR},
-              { expand: true, cwd: SOURCE_DIR, src: ['robots.txt'], dest: BUILD_DIR},
-              { expand: true, cwd: SOURCE_DIR, src: ['sitemap.xml'], dest: BUILD_DIR},
-              { expand: true, cwd: SOURCE_DIR, src: ['cv.pdf'], dest: BUILD_DIR}
-            ]
-          }
         }
     });
 
-
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-
     // Default task(s).
-    grunt.registerTask('default', ['clean', 'uglify', 'cssmin', 'copy']);
+    grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
